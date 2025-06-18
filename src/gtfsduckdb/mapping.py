1b58a0f2dc253764774068462046ddc5700c62e6
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-06-18 11:12:52 +0200

    improved logging for mapping module, references #29

--- src/gtfsduckdb/mapping.py
+++ src/gtfsduckdb/mapping.py
@@ -5,6 +5,7 @@ def map_id(id:str, mapping:dict) -> str|None:
 
     for key, value in mapping.items():
         if re.match(key, id):
+            logging.info(f"{key} does match {value}")
             return value
         else:
             logging.warning(f"{key} does not match {value}")
