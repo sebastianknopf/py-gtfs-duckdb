88ae02d8ae206b1b00525b3a28dd5be911c1ba94
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-06-18 11:08:36 +0200

    added extended logging for debugging purposes, references #29

--- src/gtfsduckdb/mapping.py
+++ src/gtfsduckdb/mapping.py
@@ -1,9 +1,12 @@
 import re
+import logging
 
 def map_id(id:str, mapping:dict) -> str|None:
 
     for key, value in mapping.items():
         if re.match(key, id):
             return value
+        else:
+            logging.warning(f"{key} does not match {value}")
         
     return id
