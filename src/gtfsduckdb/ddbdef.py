6bad809f742ccb8def080b87507cde0df2b4dfd1
sebastianknopf <mail@sebastian-knopf.de>
2025-06-30 10:55:50 +0200

    updated ddbdef with TTE extension tables, references #37

--- src/gtfsduckdb/ddbdef.py
+++ src/gtfsduckdb/ddbdef.py
@@ -2,54 +2,54 @@ schema = {
     'agency': """
     CREATE TABLE IF NOT EXISTS agency
     (
-        agency_id                  TEXT NOT NULL,
-            agency_name            TEXT NOT NULL,
-            agency_url             TEXT NOT NULL,
-            agency_timezone        TEXT NOT NULL,
-            agency_lang            TEXT,
-            agency_phone           TEXT,
-            agency_fare_url        TEXT,
-            agency_email           TEXT,
-            PRIMARY KEY ( agency_id )
+        agency_id              TEXT NOT NULL,
+        agency_name            TEXT NOT NULL,
+        agency_url             TEXT NOT NULL,
+        agency_timezone        TEXT NOT NULL,
+        agency_lang            TEXT,
+        agency_phone           TEXT,
+        agency_fare_url        TEXT,
+        agency_email           TEXT,
+        PRIMARY KEY ( agency_id )
     )
     """,
     'calendar_dates': """
     CREATE TABLE IF NOT EXISTS calendar_dates
     (
-        service_id					TEXT NOT NULL,
-        date						INTEGER NOT NULL,
-        exception_type				INTEGER NOT NULL,
+        service_id				TEXT NOT NULL,
+        date					INTEGER NOT NULL,
+        exception_type			INTEGER NOT NULL,
         PRIMARY KEY ( service_id, date )
     )
     """,
     'calendar': """
     CREATE TABLE IF NOT EXISTS calendar
     (
-        service_id					TEXT NOT NULL,
-        monday						INTEGER NOT NULL,
+        service_id				TEXT NOT NULL,
+        monday					INTEGER NOT NULL,
         tuesday					INTEGER NOT NULL,
-        wednesday					INTEGER NOT NULL,
-        thursday					INTEGER NOT NULL,
-        friday						INTEGER NOT NULL,
-        saturday					INTEGER NOT NULL,
-        sunday						INTEGER NOT NULL,
+        wednesday				INTEGER NOT NULL,
+        thursday				INTEGER NOT NULL,
+        friday					INTEGER NOT NULL,
+        saturday				INTEGER NOT NULL,
+        sunday					INTEGER NOT NULL,
         start_date 				INTEGER NOT NULL,
-        end_date 					INTEGER NOT NULL,
+        end_date 				INTEGER NOT NULL,
         PRIMARY KEY ( service_id )
     )
     """,
     'feed_info': """
     CREATE TABLE IF NOT EXISTS feed_info
     (
-        feed_publisher_name TEXT NOT NULL,
-        feed_publisher_url  TEXT NOT NULL,
-        feed_lang           TEXT NOT NULL,
-        default_lang        TEXT,
-        feed_start_date     INTEGER,
-        feed_end_date       INTEGER,
-        feed_version        TEXT,
-        feed_contact_email  TEXT,
-        feed_contact_url    TEXT
+        feed_publisher_name     TEXT NOT NULL,
+        feed_publisher_url      TEXT NOT NULL,
+        feed_lang               TEXT NOT NULL,
+        default_lang            TEXT,
+        feed_start_date         INTEGER,
+        feed_end_date           INTEGER,
+        feed_version            TEXT,
+        feed_contact_email      TEXT,
+        feed_contact_url        TEXT
     )
     """,
     'routes': """
@@ -58,15 +58,15 @@ schema = {
         agency_id 				TEXT NOT NULL,
         route_id				TEXT,
         route_short_name		TEXT,
-        route_long_name		TEXT,
+        route_long_name		    TEXT,
         route_desc				TEXT,
         route_type				INTEGER NOT NULL,
         route_url				TEXT,
-        route_color			TEXT,
+        route_color			    TEXT,
         route_text_color		TEXT,
         route_sort_order		INTEGER,
         continuous_pickup		INTEGER,
-        continuous_drop_off	INTEGER,
+        continuous_drop_off	    INTEGER,
         network_id				TEXT,
         PRIMARY KEY ( route_id )
     ) 
@@ -74,11 +74,11 @@ schema = {
     'shapes': """
     CREATE TABLE IF NOT EXISTS shapes
     (
-        shape_id            TEXT NOT NULL,
-        shape_pt_lat        FLOAT NOT NULL,
-        shape_pt_lon        FLOAT NOT NULL,
-        shape_pt_sequence   INTEGER NOT NULL,
-        shape_dist_traveled FLOAT
+        shape_id                TEXT NOT NULL,
+        shape_pt_lat            FLOAT NOT NULL,
+        shape_pt_lon            FLOAT NOT NULL,
+        shape_pt_sequence       INTEGER NOT NULL,
+        shape_dist_traveled     FLOAT
     )
     """,
     'stop_times': """
@@ -109,20 +109,20 @@ schema = {
     CREATE TABLE IF NOT EXISTS stops
     (
         stop_id						TEXT NOT NULL,
-        stop_code						TEXT,
-        stop_name						TEXT,
-        tts_stop_name					TEXT,
-        stop_desc						TEXT,
-        stop_lat						FLOAT,
-        stop_lon						FLOAT,
+        stop_code					TEXT,
+        stop_name					TEXT,
+        tts_stop_name				TEXT,
+        stop_desc					TEXT,
+        stop_lat					FLOAT,
+        stop_lon					FLOAT,
         zone_id						TEXT,
-        stop_url 						TEXT,
-        location_type 					TEXT,
-        parent_station					TEXT,
-        stop_timezone					TEXT,
+        stop_url 					TEXT,
+        location_type 				TEXT,
+        parent_station				TEXT,
+        stop_timezone				TEXT,
         wheelchair_boarding			TEXT,
-        level_id						TEXT,
-        platform_code					TEXT,
+        level_id					TEXT,
+        platform_code				TEXT,
         PRIMARY KEY ( stop_id )
     )
     """,
@@ -142,17 +142,36 @@ schema = {
     'trips': """
     CREATE TABLE IF NOT EXISTS trips
     (
-        route_id				TEXT NOT NULL,
-        service_id				TEXT NOT NULL,
-        trip_id				TEXT NOT NULL,
-        trip_headsign			TEXT,
-        trip_short_name		TEXT,
+        route_id			    TEXT NOT NULL,
+        service_id			    TEXT NOT NULL,
+        trip_id				    TEXT NOT NULL,
+        trip_headsign		    TEXT,
+        trip_short_name		    TEXT,
         direction_id			TEXT,
         block_id				TEXT,
         shape_id				TEXT,
         wheelchair_accessible	TEXT,
         bikes_allowed			TEXT,
         PRIMARY KEY ( trip_id )
+    )
+    """,
+    'ticketing_deep_links': """
+    CREATE TABLE IF NOT EXISTS ticketing_deep_links
+    (
+        ticketing_deep_link_id  TEXT NOT NULL,
+        web_url			        TEXT,
+        android_intent_uri	    TEXT,
+        ios_universal_link_url  TEXT,
+        PRIMARY KEY ( ticketing_deep_link_id )
+    )
+    """,
+    'ticketing_identifiers': """
+    CREATE TABLE IF NOT EXISTS ticketing_identifiers
+    (
+        ticketing_stop_id  TEXT NOT NULL,
+        stop_id			   TEXT,
+        agency_id	       TEXT,
+        PRIMARY KEY ( stop_id, agency_id )
     )
     """,
     'realtime_vehicle_positions': """
@@ -185,76 +204,76 @@ schema = {
     'realtime_trip_updates': """
     CREATE TABLE IF NOT EXISTS realtime_trip_updates
     (
-        trip_update_id TEXT NOT NULL,
-        trip_id TEXT,
-        trip_route_id TEXT,
-        trip_direction_id TEXT,
-        trip_start_time TEXT,
-        trip_start_date TEXT,
-        trip_schedule_relationship TEXT,
-        vehicle_id TEXT,
-        vehicle_label TEXT,
-        vehicle_license_plate TEXT,
-        vehicle_wheelchair_accessible TEXT,
-        timestamp INTEGER,
-        last_updated_timestamp        TIMESTAMP DEFAULT current_timestamp
+        trip_update_id                  TEXT NOT NULL,
+        trip_id                         TEXT,
+        trip_route_id                   TEXT,
+        trip_direction_id               TEXT,
+        trip_start_time                 TEXT,
+        trip_start_date                 TEXT,
+        trip_schedule_relationship      TEXT,
+        vehicle_id                      TEXT,
+        vehicle_label                   TEXT,
+        vehicle_license_plate           TEXT,
+        vehicle_wheelchair_accessible   TEXT,
+        timestamp                       INTEGER,
+        last_updated_timestamp          TIMESTAMP DEFAULT current_timestamp
     )
     """,
     'realtime_trip_stop_time_updates': """
     CREATE TABLE IF NOT EXISTS realtime_trip_stop_time_updates
     (
-        trip_update_id        TEXT NOT NULL,
-        stop_sequence         INTEGER,
-        stop_id               TEXT,
-        arrival_time          INTEGER,
-        arrival_delay         INTEGER,
-        arrival_uncertainty   INTEGER,
-        departure_time        INTEGER,
-        departure_delay       INTEGER,
-        departure_uncertainty INTEGER,
-        schedule_relationship TEXT,
-        last_updated_timestamp TIMESTAMP DEFAULT current_timestamp
+        trip_update_id          TEXT NOT NULL,
+        stop_sequence           INTEGER,
+        stop_id                 TEXT,
+        arrival_time            INTEGER,
+        arrival_delay           INTEGER,
+        arrival_uncertainty     INTEGER,
+        departure_time          INTEGER,
+        departure_delay         INTEGER,
+        departure_uncertainty   INTEGER,
+        schedule_relationship   TEXT,
+        last_updated_timestamp  TIMESTAMP DEFAULT current_timestamp
     )
     """,
     'realtime_service_alerts': """
     CREATE TABLE IF NOT EXISTS realtime_service_alerts
     (
-        service_alert_id     TEXT NOT NULL,
-        cause                TEXT,
-        effect               TEXT,
-        url                  TEXT,
-        header_text          TEXT NOT NULL,
-        description_text     TEXT NOT NULL,
-        tts_header_text      TEXT,
-        tts_description_text TEXT,
-        severity_level       TEXT,
-        last_updated_timestamp TIMESTAMP DEFAULT current_timestamp
+        service_alert_id        TEXT NOT NULL,
+        cause                   TEXT,
+        effect                  TEXT,
+        url                     TEXT,
+        header_text             TEXT NOT NULL,
+        description_text        TEXT NOT NULL,
+        tts_header_text         TEXT,
+        tts_description_text    TEXT,
+        severity_level          TEXT,
+        last_updated_timestamp  TIMESTAMP DEFAULT current_timestamp
     )
     """,
     'realtime_alert_active_periods': """
     CREATE TABLE IF NOT EXISTS realtime_alert_active_periods
     (
-        service_alert_id TEXT NOT NULL,
-        start_timestamp  INTEGER,
-        end_timestamp    INTEGER,
-        last_updated_timestamp TIMESTAMP DEFAULT current_timestamp
+        service_alert_id        TEXT NOT NULL,
+        start_timestamp         INTEGER,
+        end_timestamp           INTEGER,
+        last_updated_timestamp  TIMESTAMP DEFAULT current_timestamp
     )
     """,
     'realtime_alert_informed_entities': """
     CREATE TABLE IF NOT EXISTS realtime_alert_informed_entities
     (
-        service_alert_id           TEXT NOT NULL,
-        agency_id                  TEXT,
-        route_id                   TEXT,
-        route_type                 INTEGER,
-        trip_id                    TEXT,
-        trip_route_id              TEXT,
-        trip_direction_id          TEXT,
-        trip_start_time            TEXT,
-        trip_start_date            TEXT,
-        trip_schedule_relationship TEXT,
-        stop_id                    TEXT,
-        last_updated_timestamp TIMESTAMP DEFAULT current_timestamp
+        service_alert_id            TEXT NOT NULL,
+        agency_id                   TEXT,
+        route_id                    TEXT,
+        route_type                  INTEGER,
+        trip_id                     TEXT,
+        trip_route_id               TEXT,
+        trip_direction_id           TEXT,
+        trip_start_time             TEXT,
+        trip_start_date             TEXT,
+        trip_schedule_relationship  TEXT,
+        stop_id                     TEXT,
+        last_updated_timestamp      TIMESTAMP DEFAULT current_timestamp
     )
     """
 }
